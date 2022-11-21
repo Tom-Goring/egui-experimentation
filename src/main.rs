22245851171c1453fa594e1a4db1186f6babdd9a
Tom Goring <mail@tomgoring.co.uk>
2022-11-21 23:33:05 +0000

    Add docking

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use chrono::{DateTime, Local, Utc, NaiveDateTime};
 use eframe::egui;
-use egui::plot::{Line, Plot, PlotPoints};
+use egui::{plot::{Line, Plot, PlotPoints}, TextStyle, ScrollArea};
+use egui_dock::{DockArea, Style, Tree};
 
 use core::time;
 use std::{thread, sync::{Mutex, Arc}, collections::HashMap};
@@ -10,8 +11,56 @@ type Wrapped<T> = Arc<Mutex<T>>;
 type SignalData = BoundedVecDeque<[f64; 2]>;
 type DataMap = HashMap<String, SignalData>;
 
-struct App {
+struct TabViewer {
     data: Wrapped<DataMap>
+}
+
+impl egui_dock::TabViewer for TabViewer {
+    type Tab = String;
+
+    fn ui(&mut self, ui: &mut egui::Ui, tab: &mut Self::Tab) {
+        ui.label(format!("Content of {tab}"));
+        match tab.as_str() {
+            "tab1" => {
+                let data = self.data.lock().unwrap();
+                for (k, v) in data.iter() {
+                    ui.add(egui::Label::new(k));
+                    let data: Vec<[f64; 2]> = v.clone().iter().cloned().collect();
+                    let points = PlotPoints::from_iter(data);
+                    Plot::new(k).view_aspect(2.0).x_axis_formatter(|x, _range| { 
+                        let datetime = NaiveDateTime::from_timestamp_millis(x as i64).unwrap();
+                        let timestamp = DateTime::<Local>::from(DateTime::<Utc>::from_utc(datetime, Utc));
+                        format!("{}", timestamp.format("%H:%M:%S"))
+                    }).label_formatter(|_, xy| {
+                        let datetime = NaiveDateTime::from_timestamp_millis(xy.x as i64).unwrap();
+                        let timestamp = DateTime::<Local>::from(DateTime::<Utc>::from_utc(datetime, Utc));
+                        format!("x: {}\ny: {}", timestamp.format("%H:%M:%S"), xy.y) 
+                    }).show(ui, |plot_ui| plot_ui.line(Line::new(points)));
+                }
+            },
+            "tab2" => {
+                let text_style = TextStyle::Body;
+                let row_height = ui.text_style_height(&text_style);
+                let num_rows = 10_000;
+                ScrollArea::vertical().id_source("Scroll Test").auto_shrink([false; 2]).show_rows(ui, row_height, num_rows, |ui, row_range| {
+                    for row in row_range {
+                        let text = format!("This is row {}/{}", row + 1, num_rows);
+                        ui.label(text);
+                    }
+                });
+            },
+            _ => unreachable!()
+        }
+    }
+
+    fn title(&mut self, tab: &mut Self::Tab) -> egui::WidgetText {
+        (&*tab).into()
+    }
+}
+
+struct App {
+    data: Wrapped<DataMap>,
+    tree: Tree<String>
 }
 
 impl App {
@@ -24,8 +73,11 @@ impl App {
 
         }
 
+        let tree = Tree::new(vec!["tab1".to_owned(), "tab2".to_owned()]);
+
         let app = App {
-            data: data.clone()
+            data: data.clone(),
+            tree
         };
 
         let ctx = cc.egui_ctx.clone();
@@ -56,27 +108,9 @@ impl App {
 
 impl eframe::App for App {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        egui::CentralPanel::default().show(ctx, |ui| {
+        egui::CentralPanel::default().show(ctx, |_ui| {
             {
-                let data = self.data.lock().unwrap();
-                egui::ScrollArea::vertical().show(ui, |ui| {
-                    for (k, v) in data.iter() {
-                        ui.add(egui::Label::new(k));
-                        let data: Vec<[f64; 2]> = v.clone().iter().cloned().collect();
-                        let points = PlotPoints::from_iter(data);
-                        Plot::new(k).view_aspect(2.0).x_axis_formatter(|x, _range| { 
-                            let datetime = NaiveDateTime::from_timestamp_millis(x as i64).unwrap();
-                            let timestamp = DateTime::<Local>::from(DateTime::<Utc>::from_utc(datetime, Utc));
-                            format!("{}", timestamp.format("%H:%M:%S"))
-                        }).label_formatter(|_, xy| {
-                            let datetime = NaiveDateTime::from_timestamp_millis(xy.x as i64).unwrap();
-                            let timestamp = DateTime::<Local>::from(DateTime::<Utc>::from_utc(datetime, Utc));
-                            format!("x: {}\ny: {}", timestamp.format("%H:%M:%S"), xy.y) 
-                        }).show(ui, |plot_ui| plot_ui.line(Line::new(points)));
-                    }
-                });
-                // let x = &mut self.inner.lock().unwrap().amplitude;
-                // ui.add(egui::Slider::new(x, 0_f64..=100_f64).text("Amplitude"));
+                DockArea::new(&mut self.tree).style(Style::from_egui(ctx.style().as_ref())).show(ctx, &mut TabViewer { data: self.data.clone() });
             }
         });
     }

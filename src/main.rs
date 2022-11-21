6fa38e7238944d948b6b5a7f2ed1e9850d464ba6
Tom Goring <mail@tomgoring.co.uk>
2022-11-21 00:01:23 +0000

    Add timestamps

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,47 @@
+use chrono::{DateTime, Local, Utc, NaiveDateTime};
 use eframe::egui;
 use egui::plot::{Line, Plot, PlotPoints};
 
 use core::time;
-use std::{thread, sync::{Mutex, Arc}};
+use std::{thread, sync::{Mutex, Arc}, collections::HashMap, time::{Instant, Duration}};
+use std::time::{SystemTime, UNIX_EPOCH};
 
-struct Inner {
-    amplitude: f64,
-}
-
-impl Inner {
-    pub fn new(amplitude: f64) -> Arc<Mutex<Self>> {
-        Arc::new(Mutex::new(Inner { amplitude }))
-    }
-}
+type Wrapped<T> = Arc<Mutex<T>>;
+type SignalData = BoundedVecDeque<[f64; 2]>;
+type DataMap = HashMap<String, SignalData>;
 
 struct App {
-    data: Arc<Mutex<BoundedVecDeque<[f64; 2]>>>,
-    inner: Arc<Mutex<Inner>>
+    data: Wrapped<DataMap>
 }
 
 impl App {
     pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
-        let data: BoundedVecDeque<[f64; 2]> = BoundedVecDeque::new(1000);
-        let mutex = Arc::new(Mutex::new(data));
-        let inner = Inner::new(1.0);
+        let data = Arc::new(Mutex::new(DataMap::new()));
+
+        {
+            let d = &mut data.lock().unwrap();
+            d.insert("Hello".into(), BoundedVecDeque::new(1000));
+
+        }
 
         let app = App {
-            data: mutex.clone(),
-            inner: inner.clone()
+            data: data.clone()
         };
 
         let ctx = cc.egui_ctx.clone();
 
         thread::spawn(move || {
-            let mut x = 0.0;
+            let mut x: f64 = 0.0;
             loop {
                 {
-                    if let Ok(mut data_lock) = mutex.try_lock() {
-                        if let Ok(lock) = inner.try_lock() {
-                            let amplitude = lock.amplitude;
-                            data_lock.push_back([x, amplitude * x.sin()]);
+                    if let Ok(mut data) = data.try_lock() {
+                        for (_, v) in data.iter_mut() {
+                            let now_as_millis = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_millis();
+                            let datetime = NaiveDateTime::from_timestamp_millis(now_as_millis as i64).unwrap();
+                            let timestamp = DateTime::<Local>::from(DateTime::<Utc>::from_utc(datetime, Utc));
+                            println!("{}", timestamp.format("%H:%M:%S"));
+                            let n = now_as_millis as f64;
+                            v.push_back([n, x.sin()]);
                         }
                     }
                 }
@@ -59,11 +61,21 @@ impl eframe::App for App {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(&ctx, |ui| {
             {
-                let x = &mut self.inner.lock().unwrap().amplitude;
-                ui.add(egui::Slider::new(x, 0_f64..=100_f64).text("Amplitude"));
-                let data: Vec<[f64; 2]> = self.data.lock().unwrap().clone().iter().cloned().collect();
-                let points = PlotPoints::from_iter(data);
-                Plot::new("Plot").view_aspect(2.0).show(ui, |plot_ui| plot_ui.line(Line::new(points)));
+                let data = self.data.lock().unwrap();
+                egui::ScrollArea::vertical().show(ui, |ui| {
+                    for (k, v) in data.iter() {
+                        ui.add(egui::Label::new(k));
+                        let data: Vec<[f64; 2]> = v.clone().iter().cloned().collect();
+                        let points = PlotPoints::from_iter(data);
+                        Plot::new(k).view_aspect(2.0).x_axis_formatter(|x, _range| { 
+                            let datetime = NaiveDateTime::from_timestamp_millis(x as i64).unwrap();
+                            let timestamp = DateTime::<Local>::from(DateTime::<Utc>::from_utc(datetime, Utc));
+                            format!("{}", timestamp.format("%H:%M:%S"))
+                        }).show(ui, |plot_ui| plot_ui.line(Line::new(points)));
+                    }
+                });
+                // let x = &mut self.inner.lock().unwrap().amplitude;
+                // ui.add(egui::Slider::new(x, 0_f64..=100_f64).text("Amplitude"));
             }
         });
     }
